a9c93732d7a7ca99a0aeb91ce13629de59545a3d
sarcoma <sarcoma@live.co.uk>
2019-03-15 21:57:38 +0000

    kwargs passed to script as a deepcopy

--- script_benchmark_tools/benchmark.py
+++ script_benchmark_tools/benchmark.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from script_benchmark_tools.result import Result
 from script_benchmark_tools.timer import timer
 
@@ -5,7 +7,7 @@ from script_benchmark_tools.timer import timer
 def benchmark(script, kwargs, n_runs=1000):
     run_times = []
     for _ in range(n_runs):
-        time = timer(script, kwargs)
+        time = timer(script, deepcopy(kwargs))
         run_times.append(time)
 
     return Result(script, run_times, n_runs)
